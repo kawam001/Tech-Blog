26d4bcd5f60e3e9f8ebb7f920d19632d0fc6387d
James.Kamau <james.kamau>
2023-11-13 04:24:19 -0500

    amended images, icons and tabs

--- seeds/userData.js
+++ seeds/userData.js
@@ -3,23 +3,23 @@ const { User } = require('../models');
 const userData = [
   {
     "username": "Admin1",
-    "password": "Apsw1!"
-  },
-  {
-    "username": "Admin2",
-    "password": "Apsw2!"
+    "password": "Password123!"
   },
   {
     "username": "User1",
-    "password": "Upsw1!"
+    "password": "Password123!"
   },
   {
     "username": "User2",
-    "password": "Upsw2!"
+    "password": "Password123!"
   },
   {
     "username": "User3",
-    "password": "Upsw3!"
+    "password": "Password123!"
+  },
+  {
+    "username": "User4",
+    "password": "Password123!"
   }
 ]
 
